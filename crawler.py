0aee7e422251430b2b74d552ef71a47dbf3c3c20
Amey Kusurkar <amey1000@gmail.com>
2016-12-30 19:27:56 +0000

    CrawlParser passes simple tests

--- crawler.py
+++ crawler.py
@@ -1,47 +1,4 @@
-from urllib2 import urlopen, URLError
-from HTMLParser import HTMLParser
-from urlparse import urljoin
-
-# Given a URL, parser finds all the links and static assets on the page
-class CrawlParser(HTMLParser):
-
-  def __init__(self, base_url):
-    self.base_url = base_url
-    self.href_links = []
-    self.assets = []
-    HTMLParser.__init__(self)
-
-  def handle_starttag(self, tag, attrs):
-    if tag == 'a':
-      self.add_attr('href', attrs, self.href_links)
-    if tag == 'link':
-      self.add_attr('stylesheet', attrs, self.assets)
-    if tag == 'script' or tag == 'img':
-      self.add_attr('src', attrs, self.assets)
-
-  def add_attr(self, attr_name, attrs, dest):
-    data = self.get_attr_value(attr_name, attrs)
-    if data:
-      full_url = urljoin(base_url, data)
-      dest.append(full_url)
-
-  def get_attr_value(self, target_attr, attrs):
-    for attr, value in attrs:
-      if attr == target_attr:
-        return value
-    return None
-
-  def crawl(self, url):
-    try:
-      content = urlopen(url).read()
-      self.feed(content)
-      return self.href_links, self.assets
-    except URLError:
-      return []
-    except ValueError:
-      raise
-
-##### SCRIPT #####
+from crawlparser import CrawlParser
 
 base_url = "http://gocardless.com"
 parser = CrawlParser(base_url)
--- test.py
+++ test.py
@@ -1,8 +1,26 @@
 import unittest
+from crawlparser import CrawlParser
 
-class TestStuff(unittest.TestCase):
-  def test_add(self):
-    self.assertEqual(3+4, 7)
+class TestCrawler(unittest.TestCase):
+
+  def test_invalid_url_raises_exception(self):
+    # base_url is not important here
+    base_url = "http://example.com"
+    parser = CrawlParser(base_url)
+    with self.assertRaises(ValueError):
+      url = "This is not a valid URL"
+      parser.crawl(url)
+
+  def test_crawl_one_image_one_link(self):
+    base_url = "http://www.doc.ic.ac.uk/~avk13"
+    url = base_url
+    parser = CrawlParser(base_url)
+    expected_links = ["http://www.doc.ic.ac.uk/project/2014/163/g1416332/#introduction"]
+    expected_assets = ["http://www.doc.ic.ac.uk/~avk13/2.jpg"]
+    expected_result = (expected_links, expected_assets)
+    result = parser.crawl(url)
+    self.assertEquals(result, expected_result)
+
 
 if __name__ == '__main__':
   unittest.main()
