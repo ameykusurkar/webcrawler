acf9ed045ed24958a6f49d90f57aea70b1db56eb
Amey Kusurkar <amey1000@gmail.com>
2016-12-30 16:40:48 +0000

    Implemented a rudimentary CrawlParser that finds all links and static assets on a given url

--- crawler.py
+++ crawler.py
@@ -1,26 +1,68 @@
-import urllib2
+from urllib2 import urlopen, URLError
 from HTMLParser import HTMLParser
+from urlparse import urljoin
 
+# Given a URL, parser finds all the links and static assets on the page
 class CrawlParser(HTMLParser):
-  href_links = []
+
+  def __init__(self, base_url):
+    self.base_url = base_url
+    self.href_links = []
+    self.assets = []
+    HTMLParser.__init__(self)
 
   def handle_starttag(self, tag, attrs):
     if tag == 'a':
-      self.store_href_link(attrs)
+      link = self.get_attr_value('href', attrs)
+      if link:
+        full_url = urljoin(base_url, link)
+        self.href_links.append(full_url)
+    if tag == 'link':
+      stylesheet = self.get_attr_value('stylesheet', attrs)
+      if stylesheet:
+        full_url = urljoin(base_url, stylesheet)
+        self.assets.append(full_url)
+    if tag == 'script':
+      script = self.get_attr_value('src', attrs)
+      if script:
+        full_url = urljoin(base_url, script)
+        self.assets.append(full_url)
+    if tag == 'img':
+      image = self.get_attr_value('src', attrs)
+      if image:
+        full_url = urljoin(base_url, image)
+        self.assets.append(full_url)
 
-  def store_href_link(self, attrs):
-    for (attr, value) in attrs:
-      if attr == "href":
-        self.href_links.append(value)
+  def get_attr_value(self, target_attr, attrs):
+    for attr, value in attrs:
+      if attr == target_attr:
+        return value
+    return None
 
-  def get_href_links(self):
-    return self.href_links
+  def crawl(self, url):
+    try:
+      content = urlopen(url).read()
+      self.feed(content)
+      return self.href_links, self.assets
+    except URLError:
+      return []
+    except ValueError:
+      raise
 
-url = "http://www.google.co.uk"
-parser = CrawlParser()
-content = urllib2.urlopen(url).read()
-parser.feed(content)
-links = parser.get_href_links()
-print(len(links))
+##### SCRIPT #####
 
+base_url = "http://gocardless.com"
+parser = CrawlParser(base_url)
+links, assets = [], []
+try:
+  links, assets = parser.crawl(base_url)
+except ValueError:
+  print("Invalid URL.")
+    
+for l in links:
+  print(l)
+print("{} link(s) found.".format(len(links)))
+for a in assets:
+  print(a)
+print("{} assets(s) found.".format(len(assets)))
 
